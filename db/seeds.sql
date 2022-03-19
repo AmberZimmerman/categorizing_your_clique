32b5fbeed2e62ba542ad82066f23d4d075aa7beb
Amber Zimmerman <ajz1221@gmail.com>
2022-03-18 19:59:51 -0600

    finishing adding dummy data into seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,13 @@ VALUES (1, "Sales Lead", 10000),
        (3, "Accountant", 125000),
        (4, "Legal Team Lead", 250000),
        (4, "Lawyer", 190000);
+
+INSERT INTO employee (role_id, manager_id, first_name, last_name)
+VALUES (1, 0, "John", "Doe"),
+        (2, 0, "Mike", "Chan"),
+        (3, 0, "Ashley", "Rodriguez"),
+        (4, 0, "Kevin", "Tupik"),
+        (5, 0, "Kunal", "Singh"),
+        (6, 0, "Malia", "Brown"),
+        (7, 0, "Sarah", "Lourd"),
+        (8, 0, "Tom", "Allen");
