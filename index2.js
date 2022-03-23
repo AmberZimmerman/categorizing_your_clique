3c851c967716b161eba3b82c87d88b37a71da0ef
Amber Zimmerman <ajz1221@gmail.com>
2022-03-23 13:17:28 -0600

    adding in new department prompts and new role prompts to new index

--- index2.js
+++ index2.js
@@ -10,9 +10,63 @@ function commandMenu(questions) {
   return inquirer.prompt(questions);
 }
 
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+// Uses the print table function I created on line 4
 function viewAllDepartments() {
     printTable('viewAllDepartments')
 }
+
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// Uses the print table function I created on line 4
+function viewAllRoles() {
+    printTable('view all roles')
+}
+
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+function viewAllEmployees() {
+    printTable('view all employees')
+}
+
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+async function addDepartment() {
+    const addNewDepartment = await commandMenu([
+        {
+          type: "input",
+          message: `What is the name of the department?`,
+          name: "newDepartment",
+        },
+      ]);
+    console.log(addNewDepartment);
+}
+
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+async function addRole() {
+    const addNewRole = await commandMenu([
+        {
+          type: "input",
+          message: `What is the name of the role?`,
+          name: "newRole",
+        },
+        {
+          type: "input",
+          message: `What is the role salary?`,
+          name: "roleSalary",
+        },
+        {
+          type: "list",
+          message: `What department does the role belong to?`,
+          choices: ["Sales", "Engineering", "Finance", "Legal"],
+          name: "newRoleDepartment",
+        },
+      ]);
+    console.log(addNewRole);
+}
+
 
 // HEN I start the application
 async function start() {
